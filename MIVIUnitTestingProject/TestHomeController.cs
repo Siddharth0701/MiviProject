6c0508bfdb80221cb6eaf1115d37630eabb3f4d0
sneha20595 <sneha20595@gmail.com>
2022-07-05 10:05:49 +0530

    unit test

---
+++ MIVIUnitTestingProject/TestHomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Spice.Controllers;
+
+namespace MIVIUnitTestingProject
+{
+    public class TestHomeController
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+       [ Test]
+        //public void TestLogin()
+        //{
+        //    HomeController obj = new HomeController(null);
+        //    var result = obj.Login("snehaswan", "Singh@07") as ViewResult;
+
+        //    //Assert.Pass();
+        //    Assert.AreEqual("AdminDashboard", result.ViewName);
+        //}
+    }
+}
